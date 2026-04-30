7c835978a53e87c57ffe9d076c27ebc4e3e05b8e
Tính Trần <tranleductinh@gmail.com>
2026-05-01 00:14:05 +0700

    feat: add supervisorId field to user schema

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -64,6 +64,7 @@ const userSchema = new Schema(
     emailVerifiedAt: { type: Date, default: null },
     address: { type: String, default: null },
     phone: { type: String, default: null },
+    supervisorId: { type: Schema.Types.ObjectId, ref: "User", default: null },
   },
   {
     timestamps: true,
