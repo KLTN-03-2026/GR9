4ff10d06f3459832cc69720aa31ecb7be1b0a423
Trần Lê Đức Tính <tinhtld@d-soft.com.vn>
2026-05-12 20:25:11 +0700

    feat: Add success response for getProviderBookingsController and finalize getBookingSuccessController

--- Backend/src/controllers/booking.controller.js
+++ Backend/src/controllers/booking.controller.js
@@ -83,6 +83,7 @@ export const getBookingSuccessController = async (req, res) => {
     } catch (err) {
         return error(res, err.message, err.status, err.errorCode);
     }
+};
 /**
  * CANCEL BOOKING
  */
@@ -113,6 +114,7 @@ export const getProviderBookingsController = async (req, res) => {
     try {
         const bookings = await getProviderBookingsService(req.user._id);
 
+        return success(res, "Get provider bookings success", bookings, 200);
     } catch (err) {
         return error(res, err.message, err.status, err.errorCode);
     }
